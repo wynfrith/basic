fd3c86a2fe524e5c17c36620747e556c3bd76137
wynfrith <wangfucheng56@gmail.com>
2017-02-08 15:52:23 +0800

    线程中的异常处理

---
+++ threads/ExceptionThread.java
@@ -0,0 +1,74 @@
+package threads;
+
+import java.util.concurrent.*;
+
+public class ExceptionThread {
+	public static void print(String s) {
+		try {
+			Thread.sleep(100);
+			System.out.println("\n\n" + s);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("未指定异常处理器情况下子线程的异常并没有出入给调用它的线程,而是直接打印到了控制台");
+
+		try {
+			new Thread(new UncaughtExceptionThread()).start();
+		} catch(Exception e) {
+			System.out.println("catch a exception");
+		}
+
+		print("给线程设置异常处理器");
+		Thread t = new Thread(new UncaughtExceptionThread());
+
+		t.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+			public void uncaughtException(Thread thread, Throwable e) {
+				System.out.println("我异常处理器被捕获了->" + e);
+			}
+		});
+		t.start();
+		System.out.println("Handler::" + t.getUncaughtExceptionHandler());
+
+		print("为多个线程设置异常处理器");
+		ExecutorService pool = Executors.newCachedThreadPool(new MyThreadFactory());
+		for(int i = 0; i < 3; i++) {
+			pool.execute(new UncaughtExceptionThread());
+		}
+		pool.shutdown();
+
+
+	}
+}
+
+class UncaughtExceptionThread implements Runnable {
+	public void run() {
+		throw new RuntimeException();
+	}
+}
+
+class MyThreadFactory implements ThreadFactory {
+	public Thread newThread(Runnable r) {
+		Thread thread = new Thread(r);
+		thread.setUncaughtExceptionHandler((t, e) -> {
+			System.out.println("被处理器捕获.. " + e);
+		});
+		return thread;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
